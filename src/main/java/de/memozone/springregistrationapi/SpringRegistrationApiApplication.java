8c32e438f2833f1e136825b9738a7937fbe03085
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2023-02-20 19:19:03 +0100

    Initial commit

---
+++ src/main/java/de/memozone/springregistrationapi/SpringRegistrationApiApplication.java
@@ -0,0 +1,13 @@
+package de.memozone.springregistrationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRegistrationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRegistrationApiApplication.class, args);
+	}
+
+}
